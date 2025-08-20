09fc5963c298f5f586d3225167457d11e4bf3bd6
Dieli Helwig <dieli.helwig@sou.unijui.edu.br>
2025-08-20 20:09:49 -0300

    Teste

--- 1.py
+++ 1.py
@@ -1,7 +1,37 @@
-result = 10 + 5
-print(result)
+# Algoritmo da Amizade
+ligar = input("Digite '1' se a pessoa atendeu, ou '0' se não atendeu\n")
+while ligar == 0:
+    ligar = input("Digite '1' se a pessoa atendeu, ou '0' se não atendeu\n")
+    if ligar == 1:
+        continue
 
-result2 = result / 2
-print(result2)
+fim = ("Comece uma amizade")
 
-test = 'test'
+print("Convide para uma refeição")
+convite = int(input("Digite '1' se a pessoa aceitou, ou '0' se não aceitou\n"))
+if convite == 1:
+    print(fim)
+    exit() 
+else:
+    print("Ofereça uma bebida")
+
+bebida = int(input("Digite '1' se a pessoa aceitou a bebida, ou '0' se não aceitou\n"))
+if bebida == 1:
+    print("Pergunte se prefere café, chá ou chocolate")
+    print(fim)
+    exit()
+else:
+    n = 3
+    while n > 0:
+        n = n - 1
+        print("Pergunte sobre um interesse")
+        interesse = int(input("Digite '1' se a pessoa teve interesse, ou '0' se não teve"))
+        if interesse == 1:
+            print ("Engage em uma conversa sobre o interesse em comum")
+            print(fim)
+            break
+        elif:
+                
+        else:
+            print("Tente novamente")
+
